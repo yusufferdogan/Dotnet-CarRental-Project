0d435907a99baebcef542777c33e5c13bb0d77af
yusufferdogan <yusuffx03@gmail.com>
2024-06-12 21:11:05 +0300

    create Brand and Color Entities

---
+++ Entities/Concrete/Brand.cs
@@ -0,0 +1,9 @@
+namespace Entities.Concrete;
+
+public class Brand
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+
+}
---
+++ Entities/Concrete/Color.cs
@@ -0,0 +1,8 @@
+namespace Entities.Concrete;
+
+public class Color
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+}
