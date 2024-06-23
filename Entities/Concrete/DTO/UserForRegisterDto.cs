d298f5ac14bd7601a1a14a586429c1552ff4a876
yusufferdogan <yusuffx03@gmail.com>
2024-06-23 20:49:52 +0300

    add new dto's

---
+++ Entities/Concrete/DTO/UserForRegisterDto.cs
@@ -0,0 +1,11 @@
+using Core.Entities;
+
+namespace Entities.Concrete.DTO;
+
+public class UserForRegisterDto : IDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
