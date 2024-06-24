67e92d37bd3db99842570a7470dd0112467109d5
yusufferdogan <yusuffx03@gmail.com>
2024-06-24 13:03:39 +0300

    test rent return car functionality

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,11 +9,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        IRentalService rentalService = new RentalManager(new EfRentalDal(), new EfCarDal());
         ICarService carService = new CarManager(new EfCarDal());
-        var cars = carService.GetCarsByColorId(Guid.Parse("05b47ccb-d908-4437-bd46-072717897295")).Data;
-        foreach (var car in cars)
-        {
-            Console.WriteLine(car);
-        }
+        ICustomerService customerService = new CustomerManager(new EfCustomerDal());
+        Guid carId = Guid.Parse("a730c85b-dc0a-417f-8f2a-06912b8de354"); //carService.GetCarsDetailsByBrand("Mercedes-Benz").Data[0].CarId;
+        Guid customerId = Guid.Parse("a6f3c706-51af-4128-a4f5-0cf80e6c94dc"); // customerService.GetAll().Data.FirstOrDefault(c => c.CompanyName == "Company of Jane")
+        // .CustomerId;
+        // Console.WriteLine(carId);
+        // Console.WriteLine(customerId);
+        // var result = rentalService.RentCar(carId, customerId);
+        Guid rentId = rentalService.GetAll().Data[0].RentalId;
+        var result = rentalService.ReturnCar(rentId);
+        Console.WriteLine(result.Message);
     }
 }
